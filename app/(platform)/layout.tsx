77b079a0bf1a213b9007ffb61a0dcafecb63c24f
bbanduraa18 <bbanduraa18@gmail.com>
2024-01-30 20:53:48 +0200

    install and configure Clerk for auth

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,11 @@
+import { ClerkProvider } from '@clerk/nextjs';
+
+const PlatformLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return <ClerkProvider>{children}</ClerkProvider>;
+};
+
+export default PlatformLayout;
