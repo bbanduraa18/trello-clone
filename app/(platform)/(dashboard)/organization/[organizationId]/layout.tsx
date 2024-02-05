6ca3232745a3b205b818da5687774739684e471d
bbanduraa18 <bbanduraa18@gmail.com>
2024-02-05 11:23:19 +0200

    create title for Organization page

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,17 @@
+import { startCase } from 'lodash';
+
 import OrgControl from './_components/org-control';
+import { auth } from '@clerk/nextjs';
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  console.log(orgSlug);
+
+  return {
+    title: startCase(orgSlug || 'organization'),
+  };
+}
 
 const OrganizationIdLayout = ({
   children,
