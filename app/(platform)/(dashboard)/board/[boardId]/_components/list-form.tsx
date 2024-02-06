9b8328eed4d0294a2e4424bd8099b5c62e811d56
bbanduraa18 <bbanduraa18@gmail.com>
2024-02-06 17:20:56 +0200

    create and implement Create List Action

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -5,11 +5,15 @@ import { useEventListener, useOnClickOutside } from 'usehooks-ts';
 import { useState, useRef, ElementRef } from 'react';
 import { useParams } from 'next/navigation';
 
+import { useAction } from '@/hooks/use-action';
+import { createList } from '@/actions/create-list';
+
 import FormInput from '@/components/form/form-input';
 import FormSubmit from '@/components/form/form-submit';
 import { Button } from '@/components/ui/button';
 
 import ListWrapper from './list-wrapper';
+import { toast } from 'sonner';
 
 const ListForm = () => {
   const params = useParams();
@@ -30,6 +34,16 @@ const ListForm = () => {
     setIsEditing(false);
   };
 
+  const { execute, fieldErrors } = useAction(createList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" created`);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === 'Escape') {
       disableEditing();
@@ -39,21 +53,33 @@ const ListForm = () => {
   useEventListener('keydown', onKeyDown);
   useOnClickOutside(formRef, disableEditing);
 
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string;
+    const boardId = formData.get('boardId') as string;
+
+    execute({
+      title,
+      boardId,
+    });
+  };
+
   if (isEditing) {
     return (
       <ListWrapper>
         <form
+          action={onSubmit}
           ref={formRef}
           className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
         >
           <FormInput
             ref={inputRef}
+            errors={fieldErrors}
             id="title"
             placeholder="Enter list title..."
             className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
           />
 
-          <input hidden value={params.boardId} name="boardId" />
+          <input hidden value={params.boardId} name="boardId" readOnly />
 
           <div className="flex items-center gap-x-1">
             <FormSubmit>Add list</FormSubmit>
