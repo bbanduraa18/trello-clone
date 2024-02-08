120b7c203bd744e781e9f5e836f362ece69b2f3d
bbanduraa18 <bbanduraa18@gmail.com>
2024-02-08 21:20:48 +0200

    implement Card Items in List Item

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -3,9 +3,11 @@
 import { ElementRef, useRef, useState } from 'react';
 
 import { ListWithCards } from '@/types';
+import { cn } from '@/lib/utils';
 
 import ListHeader from './list-header';
 import CardForm from './card-form';
+import CardItem from './card-item';
 
 interface ListItemProps {
   data: ListWithCards;
@@ -32,6 +34,17 @@ const ListItem = ({ data, index }: ListItemProps) => {
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
         <ListHeader data={data} onAddCard={enableEditing} />
+
+        <ol
+          className={cn(
+            'mx-1 px-1 py-0.5 flex flex-col gap-y-2',
+            data.cards.length > 0 ? 'mt-2' : 'mt-0'
+          )}
+        >
+          {data.cards.map((card) => (
+            <CardItem key={card.id} index={index} data={card} />
+          ))}
+        </ol>
 
         <CardForm
           ref={textareaRef}
